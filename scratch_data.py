9b4d952c10b0c0091294f99717e81fc12aaca6a6
Israel Fernandez <israelfernandez96@gmail.com>
2024-12-07 17:09:04 -0800

    Added more stuff

--- scratch_data.py
+++ scratch_data.py
@@ -14,9 +14,46 @@ from sklearn.tree import DecisionTreeClassifier, export_graphviz, plot_tree
 from sklearn.model_selection import learning_curve, GridSearchCV
 from sklearn.model_selection import cross_val_score
 from sklearn import tree
-from IPython.display import display, HTML
 
 df = pd.read_csv('tsa_claims.csv')
 
 df.info()
 df.describe()
+
+print (df.dtypes)
+print(df['Claim Amount'])
+
+
+def clean_data(col):
+    cleaned_data = df[col].str.replace("$", "", regex=False).str.replace(";","").str.replace(" ", "").replace("-",float("NaN"))
+    return cleaned_data.convert_dtypes(float)
+
+df["Claim Amount"] = clean_data('Claim Amount')
+df['Close Amount'] = clean_data('Close Amount')
+print(df["Claim Amount"])
+val1 = df.isna().sum()
+
+df = df.dropna(subset = ['Close Amount'])
+df = df.dropna(subset = ['Claim Amount'])
+val2 = df.isna().sum()
+df = df.drop('Airline Name', axis=1)
+
+df = df.dropna(subset = ['Item'])
+df['Item'].value_counts()
+item_counts = df['Item'].value_counts()
+items_to_keep = item_counts[item_counts >= 1000].index
+df = df[df['Item'].isin(items_to_keep)]
+df = df[df['Item'] != 'Other']
+
+plt.figure(figsize=(10, 6))
+df['Item'].value_counts().plot.bar(rot = 45)
+plt.xticks(ha='right', fontsize=10)
+plt.tight_layout()  
+plt.title('Items Lost or Damaged by TSA')
+
+# df = pd.get_dummies(df, columns=['Item'], drop_first=True)
+
+df['Claim Amount'] = pd.to_numeric(df['Claim Amount'], errors='coerce')
+df['Close Amount'] = pd.to_numeric(df['Close Amount'], errors='coerce')
+df.groupby("Item")[['Claim Amount', 'Close Amount']].mean().plot.bar()
+print(df.info())
